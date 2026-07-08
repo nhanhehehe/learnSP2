eed5af03e23324d8c347e2f280cc7922a3e9f372
nhanhehehe <bengame2006@gmail.com>
2026-07-09 02:36:45 +0700

    vd6: -pass kh luu trong response, cung nhu save raw trong -hash algorithm md5: luon encode 1 password ve dung 1 result giong nhau. -hash algorithm hoat dong ban cach dung module -> khong revert backwards lai vd 4 % 3 =1 va 10 % 3 = 1 -> deu chia lay du = 1. -hash algorithm ordinary hoat dong: luu hash password; user enter passowrd; hash password from user ented; compare -> valid -ordinary way of hash have drawback: infinite password can be entered but 1 password hashed only -> abuse by rainbow attack: compare similarity hash passsword -> increase possibility -md5 is still usable nowadays(fast) but less using … -> using byscript -bcript: moi lan encode la mot ket qua khac nhau in construct voi hashcode; bcript embed salt vao de encode; structure: algorithm identifier, input cost(level), salt(a base64 encoding), hash string
    .tai sao phan service authentication tai sao throw appException
    .PasswordEncoder
    .BCryptPasswordEncoder
    . tai sao viet PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
    . <dependency>
    	<groupId>org.springframework.security</groupId>
    	<artifactId>spring-security-crypto</artifactId>
     </dependency>
     sử dụng cái này thay vì dependency security vi no se chan tat ca request
    .neu khong co public o dau method trong service, thi cac method from service se khong dung duoc trong controller

--- src/main/java/hoc/tot/nhan/service/AuthenticationService.java
+++ src/main/java/hoc/tot/nhan/service/AuthenticationService.java
@@ -21,11 +21,15 @@ public class AuthenticationService {
 
     UserRepository userRepository;
 
-    public Boolean authenthicate (AuthenticationRequest request) {
+    public AuthenticationResponse authenthicate (AuthenticationRequest request) {
         var user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_EXISTED));
 
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        var authenticated =  passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!authenticated) {
+
+        }
     }
 }
